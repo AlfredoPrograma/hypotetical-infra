d6e8440fe99b19c6c551197ef8300eba174802ea
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2026-02-25 17:28:56 -0400

    feat: define root terraform block

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.33"
+    }
+  }
 
+  required_version = "1.14.2"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
